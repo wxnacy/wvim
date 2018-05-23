41955d14ba2c4894d4c1311c2a041f0633639b70
wxnacy <371032668@qq.com>
2018-05-23 19:25:21 +0800

    addmodule

--- gitmodule.sh
+++ gitmodule.sh
@@ -1,4 +1,43 @@
 #!/usr/bin/evn bash
 
-git submodule add --force https://github.com/scrooloose/nerdtree bundle/nerdtree
+git submodule add --force https://github.com/scrooloose/nerdtree               bundle/nerdtree                # 显示文件菜单栏
+git submodule add --force https://github.com/Xuyuanp/nerdtree-git-plugin       bundle/nerdtree-git-plugin     # 配合nerdtree，显示文件git提交信息
+git submodule add --force https://github.com/jistr/vim-nerdtree-tabs           bundle/vim-nerdtree-tabs       # 配合nerdtree，多tab显示一个nerdtree
 
+# git submodule add --force https://github.com/tpope/vim-pathogen.git            bundle/vim-pathogen            # 更好的插件管理
+# git submodule add --force https://github.com/Valloric/YouCompleteMe            bundle/YouCompleteMe           # 代码补全
+# git submodule add --force https://github.com/altercation/vim-colors-solarized  bundle/vim-colors-solarized    # 配色方案
+# git submodule add --force https://github.com/thinca/vim-quickrun               bundle/vim-quickrun            # 快速执行
+# git submodule add --force https://github.com/bling/vim-airline                 bundle/vim-airline             # 状态栏
+# git submodule add --force https://github.com/junegunn/vim-easy-align           bundle/vim-easy-align          # 快速对齐
+# git submodule add --force https://github.com/sjl/gundo.vim                     bundle/gundo.vim               # 查看文件编辑历史记录
+# git submodule add --force https://github.com/bronson/vim-trailing-whitespace   bundle/vim-trailing-whitespace # 快速去掉行尾空格
+# git submodule add --force https://github.com/majutsushi/tagbar                 bundle/tagbar                  # 文件标签菜单
+# git submodule add --force https://github.com/scrooloose/nerdcommenter          bundle/nerdcommenter           # 快速注释
+# git submodule add --force https://github.com/docunext/closetag.vim             bundle/closetag.vim            # 编辑xml/html,自动补全闭合标签
+# git submodule add --force https://github.com/mattn/emmet-vim                   bundle/emmet-vim               # 编辑html神器
+# git submodule add --force https://github.com/jiangmiao/auto-pairs              bundle/auto-pairs              # 成对标签自动补全
+# git submodule add --force https://github.com/kshenoy/vim-signature             bundle/vim-signature           # 书签展示以及快速跳转 增强vim的书签功能
+# git submodule add --force https://github.com/Lokaltog/vim-easymotion           bundle/vim-easymotion          # 快速跳转
+# git submodule add --force https://github.com/tpope/vim-surround                bundle/vim-surround            # 快速给词添加符号环绕
+# git submodule add --force https://github.com/tpope/vim-repeat                  bundle/vim-repeat              # 重复一个插件的操作
+# git submodule add --force https://github.com/terryma/vim-expand-region         bundle/vim-expand-region       # 区域选择
+# git submodule add --force https://github.com/ctrlpvim/ctrlp.vim                bundle/ctrlp.vim               # 文件搜索
+# git submodule add --force https://github.com/tacahiroy/ctrlp-funky             bundle/ctrlp-funky             # ctrlp插件，模糊搜索当前文件的函数
+# git submodule add --force https://github.com/vim-scripts/matchit.zip           bundle/matchit.zip             # 成对标签跳转,需要光标放在div等文字上
+# git submodule add --force https://github.com/terryma/vim-multiple-cursors      bundle/vim-multiple-cursors    # 多光标操作
+# git submodule add --force https://github.com/scrooloose/syntastic              bundle/syntastic               # 语法检查
+# git submodule add --force https://github.com/dkprice/vim-easygrep.git          bundle/vim-easygrep            # 全局搜索插件
+# git submodule add --force https://github.com/mileszs/ack.vim.git               bundle/ack.vim                 # 全局搜索插件
+# git submodule add --force https://github.com/dyng/ctrlsf.vim.git               bundle/ctrlsf.vim              # 搜索增强
+# git submodule add --force https://github.com/artur-shaik/vim-javacomplete2.git bundle/vim-javacomplete2       # java                 代码不全
+# git submodule add --force https://github.com/pangloss/vim-javascript.git       bundle/vim-javascript          # javascript           语法高亮
+# git submodule add --force https://github.com/mxw/vim-jsx.git                   bundle/vim-jsx                 # react                语法高亮
+
+
+
+# work for hexo them
+# git submodule add --force https://github.com/wxnacy/hexo-theme-yilia.git hexo/themes/yilia
+
+# work for tmux
+# git submodule add --force https://github.com/tmux-plugins/tpm shells/tmux/plugins/tpm
