17c934f87f9a4ffbfb632748794604783efbc5e6
wxnacy <371032668@qq.com>
2018-05-23 10:55:10 +0000

    add

---
+++ gitmodule.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/evn bash
+
+git submodule add --force https://github.com/scrooloose/nerdtree bundle/nerdtree
+
--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 ROOT=${PWD}
+NAME=$1
 
 
 # rm  ${HOME}/.vimrc
@@ -8,5 +9,6 @@ ROOT=${PWD}
 # cp -R ${ROOT}/vimrc ${HOME}/.vimrc
 # cp -R ${ROOT}/ ${HOME}/.vim
 
-ln -sf ${ROOT}/vimrc ${HOME}/.vimrc
-ln -sf ${ROOT} ${HOME}/.vim
+# ln -sf ${ROOT}/vimrc ${HOME}/.vimrc
+ln -sf ${ROOT}/vimrc.pathogen ${HOME}/.vimrc
+# ln -sf ${ROOT} ${HOME}/.vim
