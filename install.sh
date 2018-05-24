09f024d3ccaad1d31208899db71b1d60cddb50ff
wxnacy <371032668@qq.com>
2018-05-24 00:54:43 +0000

    add-gundo

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 ROOT=${PWD}
-NAME=$1 
+NAME=$1
 
+git clone https://github.com/wxnacy/wxnacy-vim ~/.wxnacy-vim
 
 # rm  ${HOME}/.vimrc
 # rm  ${HOME}/.vim
@@ -10,6 +11,8 @@ NAME=$1
 # cp -R ${ROOT}/ ${HOME}/.vim
 
 # ln -sf ${ROOT}/vimrc ${HOME}/.vimrc
-ln -sf ${ROOT}/vimrc.pathogen ${HOME}/.vimrc
-# ln -sf ${ROOT} ${HOME}/.vim
+echo "ln -sf ${ROOT}/vimrc.pathogen ${HOME}/.vimrc"
+echo "ln -sf ${ROOT} ${HOME}/.vim"
 
+# sudo apt -y update
+# sudo apt install -y ack-grep
--- plugin.vim
+++ plugin.vim
@@ -162,3 +162,6 @@ nmap <Leader><Leader>a :Ack<space>-i<space>
 " gundo 配置
 "===============================
 nnoremap <leader>H :GundoToggle<CR>
+if has('python3')
+    let g:gundo_prefer_python3 = 1
+endif
