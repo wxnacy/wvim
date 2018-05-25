c3c0c88a4beb23b86dff0eef901f5a355e40a3ec
wxnacy <371032668@qq.com>
2018-05-25 10:19:14 +0000

    add-install-plugin

--- install.sh
+++ install.sh
@@ -9,8 +9,16 @@ git clone https://github.com/wxnacy/wvim ${ROOT}
 echo ''
 echo 'Run the following command to quickly use wvim'
 echo ''
-echo "  ln -sf ${ROOT}/vimrc ${HOME}/.vimrc"
+echo "  mv ${HOME}/.vim ${HOME}/.vim.old"
+echo "  mv ${HOME}/.vimrc ${HOME}/.vimrc.old"
 echo "  ln -sf ${ROOT} ${HOME}/.vim"
+echo "  ln -sf ${ROOT}/vimrc ${HOME}/.vimrc"
+echo ""
+echo "If you want to use plugins, Please run"
+echo ""
+echo "  cd ${ROOT} && ./install_plugin.sh"
+echo "  ln -sf ${ROOT}/vimrc.pathogen ${HOME}/.vimrc"
+
 
 # sudo apt -y update
 # sudo apt install -y ack-grep
--- install_plugin.sh
+++ install_plugin.sh
@@ -1,20 +1,14 @@
 #!/usr/bin/env bash
 
+git submodule update --init --recursive
+
 OS=`curl -L https://raw.githubusercontent.com/wxnacy/wshell/master/bin/system/check_system.sh | bash`
 OSS=(${OS})
 SYS=${OSS[0]}
 VER=${OSS[1]}
 
-ROOT=${PWD}
-NAME=$1
 
 
-# rm  ${HOME}/.vimrc
-# rm  ${HOME}/.vim
-# cp -R ${ROOT}/vimrc ${HOME}/.vimrc
-# cp -R ${ROOT}/ ${HOME}/.vim
-
-# ln -sf ${ROOT}/vimrc ${HOME}/.vimrc
-ln -sf ${ROOT}/vimrc.pathogen ${HOME}/.vimrc
-# ln -sf ${ROOT} ${HOME}/.vim
-
+if [ $SYS == 'ubuntu' ];then
+    sudo apt install -y ack-grep
+fi
