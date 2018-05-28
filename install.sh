2f598598068b6ed0609f526463dc0381f92749ec
wxnacy <371032668@qq.com>
2018-05-28 13:44:48 +0000

    fix

--- install.sh
+++ install.sh
@@ -9,15 +9,20 @@ NAME=$1
 echo ''
 echo 'Run the following command to quickly use wvim'
 echo ''
-echo "  mv ${HOME}/.vim ${HOME}/.vim.old"
-echo "  mv ${HOME}/.vimrc ${HOME}/.vimrc.old"
+if [[ -d ${HOME}/.vim || -f ${HOME}/.vim ]];then
+    echo "  mv ${HOME}/.vim ${HOME}/.vim.old"
+fi
+
+if [[ -d ${HOME}/.vimrc || -f ${HOME}/.vimrc ]];then
+    echo "  mv ${HOME}/.vimrc ${HOME}/.vimrc.old"
+fi
 echo "  ln -sf ${ROOT} ${HOME}/.vim"
 echo "  ln -sf ${ROOT}/vimrc ${HOME}/.vimrc"
 echo ""
 echo "If you want to use plugins, Please run"
 echo ""
-echo "  cd ${ROOT} && ./install_plugin.sh"
 echo "  ln -sf ${ROOT}/vimrc.pathogen ${HOME}/.vimrc"
+echo "  cd ${ROOT} && ./install_plugin.sh"
 echo ""
 
 
